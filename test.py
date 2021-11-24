843bace7d1495b1dec6ecd7c5927ed1c50927192
lcc-666 <2502247952@qq.com>
2021-11-24 22:10:40 +0800

    first

---
+++ test.py
@@ -0,0 +1,41 @@
+import random
+import tqdm
+import time
+import copy
+
+
+#插入排序
+# 定义函数 insertion_sort 接受参数 list_sort，并返回插入排序结果。
+def insertion_sort(list_sort):
+    start=time.time()
+    for i in tqdm.tqdm(range(len(list_sort)),desc='插入排序'):
+        n = i
+        while n > 0:
+            if list_sort[n] < list_sort[n - 1]:
+                list_sort[n], list_sort[n - 1] = list_sort[n - 1], list_sort[n]
+                n -= 1
+            else:
+                break
+    end=time.time()
+    print("插入排序用时%.2f"%(end-start))
+
+#选择排序
+def selection_sort(list_sort):
+    start = time.time()
+    list=[]
+    for i in tqdm.tqdm(range(len(list_sort)),desc='选择排序'):
+        list.append(min(list_sort))
+        list_sort.remove(min(list_sort))
+    end = time.time()
+    print("插入排序用时%.2f" % (end - start))
+
+
+
+num=input('请输入要排序数字的数量\n')
+list=[]
+
+for i in range(eval(num)):
+    list.append(random.randint(0,200))
+selection_sort(copy.deepcopy(list))
+insertion_sort(copy.deepcopy(list))
+input('')
