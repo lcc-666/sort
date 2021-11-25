3f8964df047fbacc7cc2ef0b08420598e62e0ac5
choge <2502247952@qq.com>
2021-11-25 20:39:21 +0800

    Updata

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from test import *
 import copy
 
 dict={1:'选择排序',2:'插入排序',3:'快速排序','q':'退出'}
-
+num=input('请输入要排序的数字数量\n')
+list=[]
+for i in range(eval(num)):
+    list.append(random.randint(0, 1000))
 while True:
-    num=input('请输入要排序的数字数量\n')
-    list=[]
-    for i in range(eval(num)):
-        list.append(random.randint(0, 1000))
     print('请选择你要进行的操作'+str(dict)+'\n')
     process=input()
     if process=='q':
