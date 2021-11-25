8def78164bb5bcc445838f8df2cc718967dbb10e
choge <2502247952@qq.com>
2021-11-25 12:19:27 +0800

    分类

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import random
-
+from test import *
+import copy
 
 dict={1:'选择排序',2:'插入排序','q':'退出'}
-
 
 while True:
     num=input('请输入要排序的数字数量\n')
     list=[]
     for i in range(eval(num)):
         list.append(random.randint(0, 1000))
-    process=input(str(dict))
+    print('请选择你要进行的操作'+str(dict)+'\n')
+    process=input()
+    if process=='q':
+        print('感谢使用')
+        break
+    if process=='1':
+        selection_sort(copy.deepcopy(list))
+    if process=='2':
+        insertion_sort(copy.deepcopy(list))
